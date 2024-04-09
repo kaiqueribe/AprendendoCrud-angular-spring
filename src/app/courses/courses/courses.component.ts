2c56e5bf701ca7dd71594960e3366f9955406b94
Kaique Ribeiro <57017906+kaiqueribe@users.noreply.github.com>
2024-04-09 16:17:14 -0300

    Criando e Editando Componetes de Cursos

---
+++ src/app/courses/courses/courses.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+import {Course} from "../model/course";
+import {CoursesService} from "../services/courses.service";
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrl: './courses.component.scss'
+})
+export class CoursesComponent implements OnInit {
+
+  courses: Course[] = [];
+  displayedColumns = ['name', 'category'];
+
+  // coursesService: CoursesService;
+
+  constructor(private coursesService: CoursesService) {
+    // this.coursesService = new CoursesService();
+  }
+
+
+  ngOnInit(): void {
+    this.courses = this.coursesService.list();
+  }
+
+}

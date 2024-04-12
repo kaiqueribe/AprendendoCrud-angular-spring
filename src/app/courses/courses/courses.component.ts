47674fcc71b58b33e0136e98823127b08b5acc95
Kaique Ribeiro <57017906+kaiqueribe@users.noreply.github.com>
2024-04-12 11:31:51 -0300

    Fazendo Alterações na coursescomponent
    Html
    Inserindo tabela com colunas Nome e Categoria,
    Inserindo Icone na coluna Caregoria
    Scss
    Estilizando Tabela Cursos,Loading Spiner

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,7 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {Course} from "../model/course";
 import {CoursesService} from "../services/courses.service";
-import {Observable} from "rxjs";
+import {catchError, Observable, of} from "rxjs";
+import {error} from "@angular/compiler-cli/src/transformers/util";
+import {MatDialog} from "@angular/material/dialog";
+import {ErrorDialogComponent} from "../../shared/components/error-dialog/error-dialog.component";
 
 @Component({
   selector: 'app-courses',
@@ -10,22 +13,36 @@ import {Observable} from "rxjs";
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Observable <Course[]> ;
+  courses$: Observable<Course[]>;
   // courses:Course[] = [];
   displayedColumns = ['name', 'category'];
 
   // coursesService: CoursesService;
 
-  constructor(private coursesService: CoursesService) {
+
+  constructor(private coursesService: CoursesService,
+              public dialog: MatDialog
+  ) {
     // this.coursesService = new CoursesService();
-    this.courses = this.coursesService.list();
+    this.courses$ = this.coursesService.list()
+      .pipe(
+        catchError(error => {
+          this.onError('Erro ao carregar os cursos');
+          return of([]);
+        })
+      );
 
     // this.coursesService.list().subscribe(courses => this.courses = courses);
   }
 
-
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
+  }
 
   ngOnInit(): void {
+
   }
 
 }

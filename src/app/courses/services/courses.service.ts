cdc2c1f656ff00e29f54330b06458bb04949bc10
Kaique Ribeiro <57017906+kaiqueribe@users.noreply.github.com>
2024-04-15 14:25:17 -0300

    Alterando endereço de consumo do front para chamar dados da API

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -8,7 +8,7 @@ import {delay, first, take, tap} from "rxjs";
 })
 export class CoursesService {
 
-  private readonly API = '/assets/courses.json';
+  private readonly API = 'api/cursos';
 
   constructor(private httpClient: HttpClient) {
   }
